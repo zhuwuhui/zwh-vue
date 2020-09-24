0f26c22c7fb0d6cda3ab00c5e0869601324f75aa
zhuwuhui <15>
2020-09-24 10:06:31 +0800

    Element前端完成

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,11 +1,19 @@
 <template>
-  <div>
-      用户名:<input type="text" v-model="loginForm.username" placeholder="请输入用户名"/>
-      <br><br>
-      密码： <input type="password" v-model="loginForm.password" placeholder="请输入密码"/>
-      <br><br>
-      <button v-on:click="login">登录</button>
-  </div>
+    <body id="poster">
+        <el-form class="login-container" label-position="left"
+               label-width="0px">
+        <h3 class="login_title">猪无悔</h3>
+        <el-form-item>
+          <el-input type="text" v-model="loginForm.userName" auto-complete="false" placeholder="账号"/>
+        </el-form-item>
+        <el-form-item>
+          <el-input type="password" v-model="loginForm.password" auto-complete="false" placeholder="密码"/>
+        </el-form-item>
+          <el-form-item style="width: 100%">
+          <el-button type="primary" style="width: 100%;background: #505458;border: none" v-on:click="login">登录</el-button>
+        </el-form-item>
+        </el-form>
+    </body>
 </template>
 
 <script>
@@ -15,7 +23,7 @@
     data () {
       return {
         loginForm: {
-          username: '',
+          userName: '',
           password: ''
         },
         responseResult: []
@@ -25,7 +33,7 @@
       login () {
         this.$axios
           .post('/login', {
-            username: this.loginForm.username,
+            userName: this.loginForm.userName,
             password: this.loginForm.password
           })
           .then(successResponse => {
@@ -40,5 +48,33 @@
     }
   }
 </script>
+<style>
+    #poster {
+    background:url("../assets/zhuwuhui.jpg") no-repeat;
+    background-position: center;
+    height: 100%;
+    width: 100%;
+    background-size: cover;
+    position: fixed;
+  }
+  body{
+    margin: 0px;
+  }
+  .login-container {
+    border-radius: 15px;
+    background-clip: padding-box;
+    margin: 90px auto;
+    width: 350px;
+    padding: 35px 35px 15px 35px;
+    background: #fff;
+    border: 1px solid #eaeaea;
+    box-shadow: 0 0 25px #cac6c6;
+  }
+  .login_title {
+    margin: 0px auto 40px auto;
+    text-align: center;
+    color: #505458;
+  }
+</style>
 
 
